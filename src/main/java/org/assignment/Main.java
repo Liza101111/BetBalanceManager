1dc8f077c4cea6172c1f4466cfd7f4d96d9d9d18
Liza <lixia101111@gmail.com>
2023-11-14 22:56:53 +0200

    Add methods for reading player data from files

--- src/main/java/org/assignment/Main.java
+++ src/main/java/org/assignment/Main.java
@@ -2,6 +2,16 @@ package org.assignment;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        PlayerRegistry playerRegistry = new PlayerRegistry();
+        MatchRegistry matchRegistry = new MatchRegistry();
+
+        BettingDataProcessor bettingDataProcessor = new BettingDataProcessor();
+        processPlayerData("C:\\Users\\lixia\\IdeaProjects\\BetBalanceManager\\src\\main\\resources\\player_data.txt",bettingDataProcessor, playerRegistry, matchRegistry);
+
     }
+    private static void processPlayerData(String filePath, BettingDataProcessor bettingDataProcessor, PlayerRegistry playerRegistry, MatchRegistry matchRegistry) {
+        bettingDataProcessor.processPlayerData(filePath, playerRegistry, matchRegistry);
+    }
+
 }

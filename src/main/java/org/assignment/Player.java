cb0c79c7329e11614ce3f26419348a45e9095bd2
Liza <lixia101111@gmail.com>
2023-11-14 11:44:32 +0200

    Added operations-Bet,Deposit,Withdraw for Player

--- src/main/java/org/assignment/Player.java
+++ src/main/java/org/assignment/Player.java
@@ -30,4 +30,32 @@ public class Player {
     public int getTotalWins() {
         return totalWins;
     }
+
+    public void deposit(int amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(int amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient funds.");
+        }
+    }
+
+    public void placeBet(int betAmount) {
+        if (betAmount > 0 && betAmount <= balance) {
+            balance -= betAmount;
+            System.out.println("Bet placed. New balance: " + balance);
+        } else {
+            System.out.println("Invalid bet amount or insufficient funds.");
+        }
+    }
+
 }

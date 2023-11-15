a38b265ef9ed67d3d895c59ad3c31a6c9ef9cbe4
Liza <lixia101111@gmail.com>
2023-11-16 01:32:23 +0200

    add getTotalBets in Match

--- src/main/java/org/assignment/Match.java
+++ src/main/java/org/assignment/Match.java
@@ -9,12 +9,14 @@ public class Match {
     private BigDecimal rateA;
     private BigDecimal rateB;
     private MatchResult result;
+    private int totalBets;
 
     public Match(UUID matchId, BigDecimal rateA, BigDecimal rateB, MatchResult result) {
         this.matchId = matchId;
         this.rateA = rateA;
         this.rateB = rateB;
         this.result = result;
+        this.totalBets = 0;
     }
     public Match(UUID matchId) {
         this(matchId, BigDecimal.ONE, BigDecimal.ONE, MatchResult.DRAW);
@@ -34,5 +36,13 @@ public class Match {
 
     public MatchResult getResult() {
         return result;
+    }
+
+    public int getTotalBets() {
+        return totalBets;
+    }
+
+    public void incrementTotalBets() {
+        totalBets++;
     }
 }

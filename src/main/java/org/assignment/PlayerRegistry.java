efdf67a23c550d40bed7374572b43148a115a9b5
Liza <lixia101111@gmail.com>
2023-11-15 20:35:39 +0200

    implement getLegitimatePlayers

--- src/main/java/org/assignment/PlayerRegistry.java
+++ src/main/java/org/assignment/PlayerRegistry.java
@@ -1,13 +1,19 @@
 package org.assignment;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PlayerRegistry {
     private Map<UUID,Player> players = new HashMap<>();
 
     public Player getPlayer(UUID playerId){
         return players.computeIfAbsent(playerId, Player::new);
+    }
+
+    public List<Player> getLegitimatePlayers() {
+        return players.values().stream()
+                .filter(player -> !player.hasIllegalAction())
+                .sorted(Comparator.comparing(player -> player.getPlayerId().toString()))
+                .collect(Collectors.toList());
     }
 }

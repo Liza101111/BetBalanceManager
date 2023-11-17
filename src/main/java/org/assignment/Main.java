c768fe13efcee2fc068b8b21593d00b23fbc5d3c
Liza <lixia101111@gmail.com>
2023-11-17 17:53:07 +0200

    Adjusted the order of processMatchData and processPlayerData

--- src/main/java/org/assignment/Main.java
+++ src/main/java/org/assignment/Main.java
@@ -7,9 +7,9 @@ public class Main {
         MatchRegistry matchRegistry = new MatchRegistry();
 
         BettingDataProcessor bettingDataProcessor = new BettingDataProcessor();
-        processPlayerData("src/main/resources/player_data.txt",bettingDataProcessor, playerRegistry, matchRegistry);
-        processMatchData("src/main/resources/match_data.txt", bettingDataProcessor, matchRegistry);
 
+        processMatchData("src/main/resources/match_data.txt", bettingDataProcessor, matchRegistry);
+        processPlayerData("src/main/resources/player_data.txt",bettingDataProcessor, playerRegistry, matchRegistry);
         bettingDataProcessor.writeResults("src/main/java/org/assignment/results.txt", playerRegistry, matchRegistry);
     }
     private static void processPlayerData(String filePath, BettingDataProcessor bettingDataProcessor, PlayerRegistry playerRegistry, MatchRegistry matchRegistry) {
@@ -19,5 +19,4 @@ public class Main {
     private static void processMatchData(String filePath, BettingDataProcessor bettingDataProcessor, MatchRegistry matchRegistry){
         bettingDataProcessor.processMatchData(filePath, matchRegistry);
     }
-
 }
